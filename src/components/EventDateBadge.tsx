57c37eadc5b70f1efa1759c0d0d46ce76e64ead7
nbr <nikolaus.brunner@protonmail.ch>
2025-11-25 16:57:37 +0100

    style(EventDateBadge): use warning variant instead of outline

--- src/components/EventDateBadge.tsx
+++ src/components/EventDateBadge.tsx
@@ -23,5 +23,5 @@ export function EventDateBadge({ daysUntil, isPast }: EventDateBadgeProps) {
     );
   }
 
-  return <Badge variant="outline font-mono">in {daysUntil} Tagen</Badge>;
+  return <Badge variant="warning">in {daysUntil} Tagen</Badge>;
 }
